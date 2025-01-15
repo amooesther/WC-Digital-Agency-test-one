f59db137fc12f174964b2ad95262dd87fe4a3805
Esther Toyin <amooesther2503@gmail.com>
2025-01-15 11:23:11 +0000

    hero adjustment

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ import dot from '../assets/dot.png';
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-screen">
+    <div className="relative w-full h-[90vh]">
       {/* Hero Background */}
       <img
         src={hero_bg}
@@ -14,40 +14,39 @@ const Hero = () => {
       />
 
       {/* Hero Content */}
-      <div className="relative flex flex-col-reverse md:flex-row items-center justify-between w-full h-full px-8">
+      <div className="relative flex flex-col-reverse md:flex-row items-center justify-center w-full h-full px-6 sm:px-8">
         {/* Hero Image */}
-        <div className="w-full md:w-1/2 mb-6 md:mb-0">
+        <div className="w-full mb-6 sm:mb-0 flex justify-center">
           <img
             src={hero_img}
             alt="Hero"
-            className="w-full max-w-[300px] md:max-w-[400px]"
+            className="w-[80%] max-w-[300px] sm:max-w-[400px]"
           />
         </div>
 
         {/* Text and Button */}
-        <div className="w-full md:w-1/2 flex flex-col items-center text-white text-center">
-          <p className="text-3xl md:text-5xl font-bold leading-tight mb-6">
+        <div className="w-full text-center text-white px-4">
+          <h2 className="text-3xl sm:text-4xl md:text-[50px] font-bold leading-tight mb-6">
             Give Your{' '}
             <span className="text-primary">
               Little One the Best Care
             </span>{' '}
             with Our Safe and Trusted Baby Products
-          </p>
-          <button className="px-8 py-3 mb-10  bg-primary text-gray-700 rounded-full text-lg font-medium hover:bg-colorThree hover:text-white transition duration-300 shadow-md">
+          </h2>
+          <button className="px-6 py-3 bg-primary text-gray-700 rounded-full text-lg font-medium hover:bg-colorThree hover:text-white transition duration-300 shadow-md">
             Shop Now
           </button>
         </div>
       </div>
 
-      
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 hidden md:block">
-  <img
-    src={dot}
-    alt="Dot"
-    className="w-8 md:w-12 opacity-80 rounded-full shadow-lg transition-transform duration-300 hover:scale-110"
-  />
-</div>
-
+      {/* Dot for small screen */}
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 sm:block hidden">
+        <img
+          src={dot}
+          alt="Dot"
+          className="w-8 sm:w-12 opacity-80 rounded-full shadow-lg transition-transform duration-300 hover:scale-110"
+        />
+      </div>
     </div>
   );
 };
